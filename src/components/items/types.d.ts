11721d658ab7533a1091226e92b26d800d086820
André Adriano <andre@dev-next.com>
2020-06-04 11:05:58 -0300

    :label: Added types to some components

---
+++ src/components/items/types.d.ts
@@ -0,0 +1,5 @@
+declare type Item = {
+  id?: number;
+  image: string;
+  title: string;
+}
---
+++ src/components/points/types.d.ts
@@ -0,0 +1,11 @@
+declare type Point = {
+  id?: number;
+  image: string;
+  name: string;
+  email: string;
+  whatsapp: string;
+  latitude: string;
+  longitude: string;
+  city: string;
+  uf: string;
+}
