eeda03881195d333e0dac9190782ae5ee12ea741
André Adriano <andre@dev-next.com>
2020-06-04 11:04:48 -0300

    :card_file_box: Changed some database parameters

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,4 +2,11 @@ import knex from 'knex';
 
 export default knex({
   client: 'pg',
+  connection: {
+    host: process.env.PGHOST,
+    port: Number(process.env.PGPORT),
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+  },
 });
